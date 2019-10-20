eba2aed62cc1a739dbff6ac63370aee02089e475
Theo Turner <theo_turner@outlook.com>
2019-10-20 09:53:55 +0100

    Agent script

---
+++ scripts/agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+get_latest() {
+    if [ ! -d $2 ]; then
+        git clone https://github.com/$1/$2.git --recursive
+        cd $2
+    else
+        cd $2
+        git pull
+    fi
+    cd ..
+}
+
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+get_latest hyperledger indy-sdk
