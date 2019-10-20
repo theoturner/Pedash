1cc7fac6f43557f0511f59619e07de5139ddd785
Theo Turner <theo_turner@outlook.com>
2019-10-20 10:14:02 +0100

    Error handling for cloudagent

--- scripts/cloudagent.sh
+++ scripts/cloudagent.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+onred='\033[41m'
+endcolor="\033[0m"
+
+# Handle errors
+set -e
+error_report() {
+    echo -e "${onred}Error: failed on line $1.$endcolor"
+}
+trap 'error_report $LINENO' ERR
+
+
 get_latest() {
     if [ ! -d $2 ]; then
-        git clone https://github.com/$1/$2.git --recursive
+        git clone https://github.com/$1/$2.git --depth 1
         cd $2
     else
         cd $2
