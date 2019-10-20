34dc2b7f1c2eaed9862c058a3d64893ac2a829d0
Theo Turner <theo_turner@outlook.com>
2019-10-20 13:58:27 +0100

    Update cloudagent script

--- scripts/cloudagent.sh
+++ scripts/cloudagent.sh
@@ -22,8 +22,13 @@ get_latest() {
     cd ..
 }
 
-curl https://sh.rustup.rs -sSf | sh
+trap `docker stop $(docker ps -a | grep oef-search | awk '{print $1}')` SIGINT
+sudo service docker start
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 get_latest hyperledger indy-sdk
-cd indy-sdk/vcx/dummy-cloud-agent
+cd indy-sdk
+sudo docker build -f ci/indy-pool.dockerfile -t indy_pool .
+sudo docker run -itd -p 9701-9708:9701-9708 indy_pool &
+cd vcx/dummy-cloud-agent
 cargo run ./config/sample-config.json
