5df15fcaaaa00925e5e089d8db272a45dec44f91
Theo Turner <theo_turner@outlook.com>
2019-10-20 10:13:50 +0100

    Error handling for installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+onred='\033[41m'
+ongreen='\033[42m'
+onyellow='\033[43m'
+endcolor="\033[0m"
+
+# Handle errors
+set -e
+error_report() {
+    echo -e "${onred}Error: failed on line $1.$endcolor"
+}
+trap 'error_report $LINENO' ERR
+
+
 if [[ "$OSTYPE" != "linux-gnu" ]]; then
-    echo "OS not supported. Pedash runs on Linux."
+    echo -e "${onred}OS not supported. Pedash runs on Linux.$endcolor"
 fi
+
+echo -e "${onyellow}Installing Pedash...$endcolor"
 
 sudo apt-get update
 sudo apt-get install build-essential git python3
@@ -11,3 +26,4 @@ sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb bionic master"
 sudo apt-get update
 sudo apt-get install -y libnullpay libvcx
 
+echo -e "${ongreen}Pedash installed.$endcolor"
