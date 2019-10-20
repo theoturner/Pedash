ca47ae137511197026dac688c9f3d8cf9bf1c5a7
Theo Turner <theo_turner@outlook.com>
2019-10-20 09:56:13 +0100

    Installer: OS check + drop rust

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" != "linux-gnu" ]]; then
+    echo "OS not supported. Pedash runs on Linux."
+fi
 
 sudo apt-get update
 sudo apt-get install build-essential git python3
-curl https://sh.rustup.rs -sSf | sh
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CE7709D068DB5E88
 sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb bionic master"
 sudo apt-get update
